59c9df62bc8965f4bf9a7167e195a9e508f1d847
Lcole.engr <lcole.engr@gmail.com>
2021-04-21 23:58:39 -0400

    initializing home screen and navbar components along with respective CSS files

---
+++ blockbuster_app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBKYZdTrpOrf0KoveDyI7JqYiIKzYyv0qU",
+    authDomain: "blockbuster-app-12572.firebaseapp.com",
+    projectId: "blockbuster-app-12572",
+    storageBucket: "blockbuster-app-12572.appspot.com",
+    messagingSenderId: "200337375825",
+    appId: "1:200337375825:web:983c48a3db206f1689ae70"
+  };
