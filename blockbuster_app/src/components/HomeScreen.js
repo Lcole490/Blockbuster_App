425407e622e53d10acbeab4fa5a0d1ae57f6e2b5
Lcole.engr <lcole.engr@gmail.com>
2021-04-23 21:09:57 -0400

    Formulated rows to list movies by genre and added browser icon to index.html

--- blockbuster_app/src/components/HomeScreen.js
+++ blockbuster_app/src/components/HomeScreen.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './HomeScreen.css'
 import Nav from './Nav'
 import Banner from './Banner'
+// import requests from '../Requests'
+import Row from './Row'
+import requests from '../Requests'
 
 function HomeScreen() {
     return (
@@ -15,6 +18,14 @@ function HomeScreen() {
             <Banner />
 
             {/* Row */}
+
+            <Row title = "Trending Now" fetchURL={requests.fetchTrending} isLargeRow/>
+            <Row title = "Top Rate" fetchURL={requests.fetchTopRated}/>
+            <Row title = "Action Movies" fetchURL={requests.fetchActionMovies}/>
+            <Row title = "Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+            <Row title = "Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+            <Row title = "Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+            <Row title = "Documentaries" fetchURL={requests.fetchDocumentaries}/>
         </div>
     )
 }
